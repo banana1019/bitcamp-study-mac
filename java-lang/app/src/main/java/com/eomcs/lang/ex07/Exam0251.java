22bc377667ae423f435b377da7ca733987bef34f
banana1019 <jyho930@gmail.com>
2021-12-19 23:58:20 +0900

    2021-12-17

--- java-lang/app/src/main/java/com/eomcs/lang/ex07/Exam0251.java
+++ java-lang/app/src/main/java/com/eomcs/lang/ex07/Exam0251.java
@@ -6,6 +6,7 @@ public class Exam0251 {
 
   // 가변 파라미터에 배열을 넘길 경우
   // 기존 배열을 그대로 사용할까? 아니면 파라미터로 받은 배열을 복제해서 사용할까?
+  // => 가변 파라미터에 배열을 넘길 경우 그 배열을 그대로 받아 사용한다.
   static void hello(String... names) {
     for (int i = 0; i < names.length; i++) {
       names[i] += "^^";
@@ -19,10 +20,10 @@ public class Exam0251 {
     String[] arr = {"김구", "안중근", "윤봉길", "유관순"};
     hello(arr);
     System.out.println("-------------------");
-    for (String value: arr) {
+    for (String value : arr) {
       System.out.println(value);
     }
 
-    //    hello("홍길동", 20, "오호라"); // 다른 타입은 안된다. 컴파일 오류!
   }
+
 }
