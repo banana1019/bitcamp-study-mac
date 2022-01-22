5a2fe82cb70aa4a5bc544d9c96b23491a48be436
banana1019 <jyho930@gmail.com>
2022-01-23 00:21:17 +0900

    2022-01-21(금) project-app1 4단계 완료

--- project-app1/app/src/main/java/com/eomcs/app1/App.java
+++ project-app1/app/src/main/java/com/eomcs/app1/App.java
@@ -2,10 +2,22 @@ package com.eomcs.app1;
 
 public class App {
   public static void main(String[] args) {
+    if (args.length == 0) {
+      System.out.println("사용법:");
+      System.out.println("App [명령] [값1] [값2]");
+      System.out.println("명령:");
+      System.out.println("add   [값1] [값2]      더하기 계산을 수행한다. 예) App add 100 200");
+      System.out.println("minus [값1] [값2]      빼기 계산을 수행한다.   예) App minus 100 200");
+      System.exit(0);
+    }
     if (args[0].equals("add")) {
-      int value1 = Integer.parseInt(args[1]);
-      int value2 = Integer.parseInt(args[2]);
-      System.out.printf("%d + %d = %d\n", value1, value2, value1 + value2);      
+      int v1 = Integer.parseInt(args[1]);
+      int v2 = Integer.parseInt(args[2]);
+      System.out.printf("%d + %d = %d\n", v1, v2, (v1 + v2));      
+    } else if (args[0].equals("minus")) {
+      int v1 = Integer.parseInt(args[1]);
+      int v2 = Integer.parseInt(args[2]);
+      System.out.printf("%d - %d = %d\n", v1, v2, (v1 - v2));      
     }
   }
 }
