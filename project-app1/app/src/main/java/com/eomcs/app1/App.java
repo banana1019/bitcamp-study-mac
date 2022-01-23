839e4e8961452c388f62b2491792ecdbc2a3e79d
banana1019 <jyho930@gmail.com>
2022-01-24 08:03:47 +0900

    2022-01-21(금) project-app1 8단계 완료

--- project-app1/app/src/main/java/com/eomcs/app1/App.java
+++ project-app1/app/src/main/java/com/eomcs/app1/App.java
@@ -1,69 +1,30 @@
 package com.eomcs.app1;
 
-import java.util.Scanner;
-
 public class App {
-
-  static Scanner keyScan = new Scanner(System.in);
 
   public static void main(String[] args) {
 
+    Console console = new Console();
+    CommandHandler commandHandler = new CommandHandler();
+
     while (true) {
-      String input = prompt();
-      if (input.equals("quit") || input.equals("exit")) {
+      Command command = console.prompt();
+      if (command.getName().equals("quit") || command.getName().equals("exit")) {
         break;
-      } else if (input.length() == 0) {
+      } else if (command.getName().equals("")) {
         continue;
-      } else if (input.equals("help")) {
-        doHelp();
+      } else if (command.getName().equals("help")) {
+        commandHandler.doHelp();
+      } else if (command.getName().equals("add")) {
+        commandHandler.doAdd(command);
+      } else if (command.getName().equals("minus")) {
+        commandHandler.doMinus(command);
       } else {
-        String[] values = input.split(" ");
-        //        System.out.println(values.length);
-        if (values[0].equals("add")) {
-          doAdd(values);
-        } else if (values[0].equals("minus")) {
-          doMinus(values);
-        } else {
-          System.out.println("지원하지 않는 연산자입니다.");
-        }
+        System.out.println("지원하지 않는 연산자입니다.");
       }
     }
 
-    keyScan.close();
+    console.close();
   }
 
-  static void doHelp() {
-    System.out.println("add   [값1] [값2]      더하기 계산을 수행한다.");
-    System.out.println("minus [값1] [값2]      빼기 계산을 수행한다.");
-    System.out.println("help                   도움말을 출력한다.");
-  }
-
-  static void doAdd(String[] values) {
-    if (values.length != 3) {
-      System.out.println("add 명령어 입력 형식이 올바르지 않습니다.");
-      System.out.println("형식: add 값1 값2");
-      System.out.println("예) add 100 200");
-    } else {
-      int v1 = Integer.parseInt(values[1]);
-      int v2 = Integer.parseInt(values[2]);
-      System.out.printf("%d + %d = %d\n", v1, v2, (v1 + v2));
-    }
-  }
-
-  static void doMinus(String[] values) {
-    if (values.length != 3) {
-      System.out.println("minus 명령어 입력 형식이 옳지 않습니다.");
-      System.out.println("형식: minus 값1 값2");
-      System.out.println("예) minus 100 200");
-    } else {
-      int v1 = Integer.parseInt(values[1]);
-      int v2 = Integer.parseInt(values[2]);
-      System.out.printf("%d - %d = %d\n", v1, v2, (v1 - v2));
-    }
-  }
-
-  static String prompt() {
-    System.out.print("> ");
-    return keyScan.nextLine();
-  }
 }
