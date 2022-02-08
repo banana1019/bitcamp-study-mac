3ef0e15d7198961f02cb1c8598939a6b22afc52e
banana1019 <jyho930@gmail.com>
2022-02-08 15:55:52 +0900

    2022=02-08(화) com.eomcs.net.ex11.test - 3

--- java-lang/app/src/main/java/com/eomcs/net/ex11/test/CalculatorServer.java
+++ java-lang/app/src/main/java/com/eomcs/net/ex11/test/CalculatorServer.java
@@ -1,13 +1,27 @@
 package com.eomcs.net.ex11.test;
 
+import java.io.PrintStream;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class CalculatorServer {
   public static void main(String[] args) throws Exception {
     try (ServerSocket serverSocket = new ServerSocket(8888)) {
       System.out.println("서버 실행 중...");
 
-      serverSocket.accept();
+      Socket socket = serverSocket.accept();
+
+      Scanner in = new Scanner(socket.getInputStream());
+      PrintStream out = new PrintStream(socket.getOutputStream());
+
+      String str = in.nextLine();
+      out.println(str);
+      out.flush();
+
+      in.close();
+      out.close();
+      socket.close();
 
     } catch (Exception e) {
       System.out.println("서버 소켓 생성 중 오류 발생!");
