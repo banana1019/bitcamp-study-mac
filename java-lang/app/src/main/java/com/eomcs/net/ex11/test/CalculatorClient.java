16b1b24b54204d56177ba9cbdb03a888156fd34a
banana1019 <jyho930@gmail.com>
2022-02-08 16:01:20 +0900

    2022-02-08(화) com.eomcs.net.ex11.test - 5

--- java-lang/app/src/main/java/com/eomcs/net/ex11/test/CalculatorClient.java
+++ java-lang/app/src/main/java/com/eomcs/net/ex11/test/CalculatorClient.java
@@ -1,11 +1,20 @@
 package com.eomcs.net.ex11.test;
 
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class CalculatorClient {
   public static void main(String[] args) throws Exception {
-    try (Socket serverSocket = new Socket("localhost", 8888)) {
-      System.out.println("서버 연결 성공!");
+    try (Socket socket = new Socket("localhost", 8888);
+        PrintStream out = new PrintStream(socket.getOutputStream());
+        Scanner in = new Scanner(socket.getInputStream());) {
+
+      out.println("test...ok!");
+      out.flush();
+
+      String str = in.nextLine();
+      System.out.println(str);
 
     } catch (Exception e) {
       System.out.println("서버 연결 오류 발생!");
