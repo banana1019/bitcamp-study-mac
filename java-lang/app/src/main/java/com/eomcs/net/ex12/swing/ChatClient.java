75b510f6fdfaff4da7de9808becd157f1c3b260c
banana1019 <jyho930@gmail.com>
2022-02-09 15:55:32 +0900

    2022-02-09(수) com.eomcs.net.ex12.awt - 17

--- java-lang/app/src/main/java/com/eomcs/net/ex12/swing/ChatClient.java
+++ java-lang/app/src/main/java/com/eomcs/net/ex12/swing/ChatClient.java
@@ -4,9 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.net.Socket;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -17,11 +17,20 @@ import javax.swing.UIManager;
 public class ChatClient extends JFrame {
   private static final long serialVersionUID = 1L;
 
+  Socket socket;
+  JTextField addressTf = new JTextField(30);
+  JTextField portTf = new JTextField(5);
+  JTextArea messageListTa = new JTextArea();
+  JTextField messageTf = new JTextField(35);
+
   public ChatClient() {
     super("계산기");
     this.addWindowListener(new WindowAdapter() {
       @Override
       public void windowClosing(WindowEvent e) {
+        try {
+          socket.close();
+        } catch (Exception ex) {}
         System.exit(0);
       }
     });
@@ -32,19 +41,13 @@ public class ChatClient extends JFrame {
     JPanel topPanel = new JPanel();
     topPanel.setLayout(new FlowLayout(FlowLayout.LEFT)); // 기본 레이아웃 관리자를 교체
 
-    JTextField addressTf = new JTextField(30);
+
     topPanel.add(addressTf);
 
-    JTextField portTf = new JTextField(5);
+
     topPanel.add(portTf);
 
     JButton connectBtn = new JButton("연결");
-    class MyActionListener implements ActionListener {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-
-      }
-    }
 
     // 1) 로컬 클래스
     //    class MyActionListener implements ActionListener {
@@ -70,13 +73,13 @@ public class ChatClient extends JFrame {
 
     contentPane.add(topPanel, BorderLayout.NORTH);
 
-    JTextArea messageListTa = new JTextArea();
+
     contentPane.add(messageListTa, BorderLayout.CENTER);
 
     JPanel bottomPanel = new JPanel();
     bottomPanel.setLayout(new FlowLayout(FlowLayout.LEFT)); // 기본 레이아웃 관리자를 교체
 
-    JTextField messageTf = new JTextField(35);
+
     bottomPanel.add(messageTf);
 
     JButton sendBtn = new JButton("보내기");
@@ -101,9 +104,20 @@ public class ChatClient extends JFrame {
 
   public void connectChatServer(ActionEvent e) {
     System.out.println("서버에 연결하기");
+    System.out.println(addressTf.getText());
+    System.out.println(portTf.getText());
+
+    try {
+      socket = new Socket(
+          addressTf.getText(), 
+          Integer.parseInt(portTf.getText()));
+    } catch (Exception ex) {
+
+    }
   }
 
   public void sendMessage(ActionEvent e) {
     System.out.println("메시지 보내기");
+    System.out.println(messageTf.getText());
   }
 }
