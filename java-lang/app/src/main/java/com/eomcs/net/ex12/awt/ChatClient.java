aa062a069a5c794c7b2e269f02ff72a792b2613e
banana1019 <jyho930@gmail.com>
2022-02-09 12:15:11 +0900

    2022-02-09(수) com.eomcs.net.ex12.awt - 4

--- java-lang/app/src/main/java/com/eomcs/net/ex12/awt/ChatClient.java
+++ java-lang/app/src/main/java/com/eomcs/net/ex12/awt/ChatClient.java
@@ -4,20 +4,22 @@ import java.awt.Frame;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class ChatClient {
-  public static void main(String[] args) {
+public class ChatClient extends Frame {
+  private static final long serialVersionUID = 1L;
 
-    class MyWindowListener extends WindowAdapter {
+  public ChatClient() {
+    super("계산기");
+    this.addWindowListener(new WindowAdapter() {
       @Override
       public void windowClosing(WindowEvent e) {
-        // 윈도우의 x 버튼을 눌렀을 때 
         System.exit(0);
       }
-    }
+    });
+    this.setSize(400, 300);
+    this.setVisible(true);
+  }
 
-    Frame f = new Frame("계산기");
-    f.addWindowListener(new MyWindowListener());
-    f.setSize(400, 300);
-    f.setVisible(true);
+  public static void main(String[] args) {
+    new ChatClient();
   }
 }
