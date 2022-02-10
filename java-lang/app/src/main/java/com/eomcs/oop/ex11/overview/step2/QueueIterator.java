7841d7df8556f0baceb9036b3dcd211fdd6db2f8
banana1019 <jyho930@gmail.com>
2022-02-10 14:52:21 +0900

    2022-02-10(목) com.eomcs.oop.ex11.overview.step2 QueueIterator 내가 함

--- java-lang/app/src/main/java/com/eomcs/oop/ex11/overview/step2/QueueIterator.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex11/overview/step2/QueueIterator.java
@@ -2,7 +2,6 @@ package com.eomcs.oop.ex11.overview.step2;
 
 public class QueueIterator implements Iterator {
   MyQueue queue;
-  int cursor;
 
   public QueueIterator(MyQueue queue) {
     this.queue = queue;
