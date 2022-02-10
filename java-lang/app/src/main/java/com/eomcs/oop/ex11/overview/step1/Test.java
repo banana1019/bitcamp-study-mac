ee6963be4369e68ac8da863e5dc0d3b19307ce67
banana1019 <jyho930@gmail.com>
2022-02-10 11:59:09 +0900

    2022-02-10(목) com.eomcs.oop.ex11.overview.step1 - 4

---
+++ java-lang/app/src/main/java/com/eomcs/oop/ex11/overview/step1/Test.java
@@ -0,0 +1,13 @@
+package com.eomcs.oop.ex11.overview.step1;
+
+public class Test {
+  public static void main(String[] args) {
+    MyList myList = new MyList();
+    myList.add("홍길동");
+    myList.add("임꺽정");
+    myList.add("유관순");
+    myList.add("안중근");    
+    myList.add("윤봉길");    
+    myList.add("김구");    
+  }
+}
