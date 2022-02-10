45be40b9e52f9b2e4c81f0da69f73924f64cbff1
banana1019 <jyho930@gmail.com>
2022-02-10 17:17:41 +0900

    2022-02-10(목) com.eomcs.oop.ex11.overview.step6 - 3

--- java-lang/app/src/main/java/com/eomcs/oop/ex11/overview/step6/MyStack.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex11/overview/step6/MyStack.java
@@ -16,10 +16,14 @@ public class MyStack extends MyList {
 
   @Override
   public Iterator iterator() {
-
+    // local class(로컬 클래스)
+    // - 메서드나 특정 블록 안에서만 사용될 클래스라면 
+    //   그 메서드나 블록에서 클래스를 정의함으로써 
+    //   명시적으로 사용 범위를 더 제한할 수 있다.
+    // - 단지 사용 범위를 더 제한한 것에 불과하다.
+    // - 로컬 클래스에도 바깥 클래스의 인스턴스 주소를 저장할 필드와 생성자가 자동으로 추가된다.
+    //
     class StackIterator implements Iterator {
-
-      int cursor;
 
       @Override
       public boolean hasNext() {
