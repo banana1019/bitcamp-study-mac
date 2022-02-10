5d600bea324b1189ae2388b8a06f703609090eaa
banana1019 <jyho930@gmail.com>
2022-02-10 12:27:51 +0900

    2022-02-10(목) com.eomcs.oop.ex11.overview.step1 - 11

--- java-lang/app/src/main/java/com/eomcs/oop/ex11/overview/step1/Test2.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex11/overview/step1/Test2.java
@@ -3,15 +3,15 @@ package com.eomcs.oop.ex11.overview.step1;
 public class Test2 {
   public static void main(String[] args) {
     MyStack myStack = new MyStack();
-    myStack.add("홍길동");
-    myStack.add("임꺽정");
-    myStack.add("유관순");
-    myStack.add("안중근");    
-    myStack.add("윤봉길");    
-    myStack.add("김구");
+    myStack.push("홍길동");
+    myStack.push("임꺽정");
+    myStack.push("유관순");
+    myStack.push("안중근");    
+    myStack.push("윤봉길");    
+    myStack.push("김구");
 
-    for (int i = 0; i < myStack.size(); i++) {
-      System.out.println(myStack.get(i));
+    while (myStack.size() > 0) {
+      System.out.println(myStack.pop());
     }
   }
 }
