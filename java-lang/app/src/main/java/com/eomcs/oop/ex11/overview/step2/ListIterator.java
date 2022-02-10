6195cc7c13347b0baeceb4bbd5aa90ece36b4209
banana1019 <jyho930@gmail.com>
2022-02-10 14:15:16 +0900

    2022-02-10(목) com.eomcs.oop.ex11.overview.step2 - 2

--- java-lang/app/src/main/java/com/eomcs/oop/ex11/overview/step2/ListIterator.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex11/overview/step2/ListIterator.java
@@ -2,6 +2,14 @@ package com.eomcs.oop.ex11.overview.step2;
 
 // MyList 컬렉션에서 데이터를 꺼내는 일을 한다.
 public class ListIterator implements Iterator {
+
+  MyList list;
+  int cursor;
+
+  public ListIterator(MyList list) {
+    this.list = list;
+  }
+
   @Override
   public boolean hasNext() {
     // TODO Auto-generated method stub
