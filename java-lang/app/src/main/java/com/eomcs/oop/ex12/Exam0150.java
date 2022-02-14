291bf24f7468d215700e400c1cd7a2f2e9d41966
banana1019 <jyho930@gmail.com>
2022-02-14 21:18:12 +0900

    2022-02-14(월) com.eomcs.algorithm.data_structure.linkedlist2 추가

--- java-lang/app/src/main/java/com/eomcs/oop/ex12/Exam0150.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex12/Exam0150.java
@@ -17,15 +17,15 @@ public class Exam0150 {
     // 2) 한 문장으로 된 표현식(=값을 리턴하는 한 문장의 코드)인 경우 괄호 생략할 수 있다.
     // => 문장은 문장인데 값을 리턴하는 문장을 '표현식(expression)' 이라 부른다.
     // => 단 괄호를 생략할 때 return 키워드도 생략해야 한다. 있으면 컴파일 오류!
-    Calculator c2 = (a, b) -> a - b;
+    Calculator c2 = (a, b) -> a + b;
     System.out.println(c2.compute(10, 20));
 
     // Math.max()는 int 값을 리턴한다. 그래서 이 메서드를 호출하는 문장은 표현식이다.
     Calculator c3 = (a, b) -> Math.max(a, b);
     System.out.println(c3.compute(10, 20));
 
-    // 값을 리턴해야 하는데 람다 문장에서 값을 리턴하지 않으면 컴파일 오류!
-    //    Calculator c4 = (a, b) -> System.out.println(a + ",", b); // 컴파일 오류!
+    // 
+    //    Calculator c4 = (a,값을 리턴해야 하는데 람다 문장에서 값을 리턴하지 않으면 컴파일 오류! b) -> System.out.println(a + ",", b); // 컴파일 오류!
     //    System.out.println(c4.compute(10, 20));
   }
 
