33049d33cb5473502aaae9555c46d09c4af4fcca
banana1019 <jyho930@gmail.com>
2022-02-14 14:49:45 +0900

    2022-02-14(월) com.eomcs.oop.ex11.h.test - 6

--- java-lang/app/src/main/java/com/eomcs/oop/ex11/h/test/LinkedList.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex11/h/test/LinkedList.java
@@ -97,7 +97,7 @@ public class LinkedList {
   public Iterator iterator() {
     // anonymous class 활용 예
     // => 오직 한 개의 인스턴스만 생성할 경우
-    // => 다른 문장 안에 넣기
+    // => return문, 할당문, 파라미터 전달하는 곳에 직접 놓기
     //
     return new Iterator() {
       int cursor;
