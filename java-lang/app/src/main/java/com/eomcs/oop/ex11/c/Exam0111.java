17cc177e1c86d20ca4b7b3a4aa625845e3b6169f
banana1019 <jyho930@gmail.com>
2022-02-14 09:54:10 +0900

    2022-02-14(월) com.eomcs.oop.ex11.h

--- java-lang/app/src/main/java/com/eomcs/oop/ex11/c/Exam0111.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex11/c/Exam0111.java
@@ -25,9 +25,9 @@ public class Exam0111 {
     // 바깥 클래스의 인스턴스를 먼저 만든 다음에 
     // inner 클래스의 인스턴스를 만든다.
     A2 outer = new A2();
-    A2.X obj = outer.new X(); // => new X(outer)
+    A2.X obj = outer.new X(); // 컴파일러는 다음 코드로 변경한다. => new X(outer)
 
     // 물론 다음과 같이 위의 두 줄을 한 줄로 표현할 수 있다.
-    A2.X obj2 = new A2().new X(); // => new X(new A2())
+    A2.X obj2 = new A2().new X(); // 컴파일러는 다음 코드로 변경한다. => new X(new A2())
   }
 }
