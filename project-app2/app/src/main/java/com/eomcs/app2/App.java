eca2fb3d5053479b1e4824eddddb8393e6159066
banana1019 <jyho930@gmail.com>
2022-02-16 11:15:43 +0900

    2022-02-16(수) project-app2 - 7

--- project-app2/app/src/main/java/com/eomcs/app2/App.java
+++ project-app2/app/src/main/java/com/eomcs/app2/App.java
@@ -23,14 +23,19 @@ public class App {
 
       switch (input) {
         case "1":
+          createScore();
           break;
         case "2":
+          listScore();
           break;
         case "3":
+          detailScore();
           break;
         case "4":
+          updateScore();
           break;
         case "5":
+          deleteScore();
           break;
         default:
           System.out.println("올바른 메뉴 번호를 입력하세요!");
@@ -57,5 +62,35 @@ public class App {
 
   private boolean checkQuit(String input) {
     return input.equals("quit") || input.equals("exit");
+  }
+
+  private void createScore() {
+    System.out.println("이름? ");
+    String name = keyScan.nextLine();
+
+    System.out.println("국어? ");
+    int kor = Integer.parseInt(keyScan.nextLine());
+
+    System.out.println("영어? ");
+    int eng = Integer.parseInt(keyScan.nextLine());
+
+    System.out.println("수학? ");
+    int math = Integer.parseInt(keyScan.nextLine());
+  }
+
+  private void listScore() {
+
+  }
+
+  private void detailScore() {
+
+  }
+
+  private void updateScore() {
+
+  }
+
+  private void deleteScore() {
+
   }
 }
