bcbf9fff8660ff0bf00c6cf33347190cab1c2c06
banana1019 <jyho930@gmail.com>
2022-02-16 15:52:33 +0900

    2022-02-16(수) project-app2-server

--- project-app2/app-server/src/main/java/com/eomcs/app2/ServerApp.java
+++ project-app2/app-server/src/main/java/com/eomcs/app2/ServerApp.java
@@ -1,58 +1,54 @@
 package com.eomcs.app2;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import com.eomcs.app2.handler.ScoreHandler;
-import com.eomcs.util.Prompt;
 
-public class App {
+public class ServerApp {
 
   ScoreHandler scoreHandler = new ScoreHandler();
 
   public static void main(String[] args) {
-    new App().service();
+    new ServerApp().service();
   }
 
   public void service() {
+    try (ServerSocket serverSocket = new ServerSocket(3306);) {
 
-    while (true) {
-      printMenu();
+      while (true) {
+        Socket socket = serverSocket.accept();
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
 
-      String input = Prompt.promptString("명령> ");
+        while (true) {
+          String command = in.readUTF();
+          if (command.equals("quit")) {
+            break;
+          }
 
-      if (checkQuit(input)) {
-        break;
+          switch (command) {
+            case "insert":
+              break;
+            case "selectList":
+              break;
+            case "selectOne":
+              break;
+            case "update":
+              break;
+            case "delete":
+              break;
+            default:
+          }
+
+        }  
       }
 
-      try {
-        switch (input) {
-          case "1": scoreHandler.create(); break;
-          case "2": scoreHandler.list(); break;
-          case "3": scoreHandler.detail(); break;
-          case "4": scoreHandler.update(); break;
-          case "5": scoreHandler.delete(); break;
-          default:
-            System.out.println("올바른 메뉴 번호를 입력하세요!");
-        }
-      } catch (Exception e) {
-        System.out.println("실행 중 오류 발생 :" + e.getMessage());
-      }
-      System.out.println();
+    } catch (Exception e) {
+      System.out.println("서버 실행 오류!");
     }
 
     System.out.println("종료!");
   }
-
-  private void printMenu() {
-    System.out.println("메뉴: ");
-    System.out.println("1. 등록");
-    System.out.println("2. 목록");
-    System.out.println("3. 상세");
-    System.out.println("4. 변경");
-    System.out.println("5. 삭제");
-  }
-
-  private boolean checkQuit(String input) {
-    return input.equals("quit") || input.equals("exit");
-  }
-
-
 }
