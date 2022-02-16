7d633543d02a813e91ff12dccc623b12d915a868
banana1019 <jyho930@gmail.com>
2022-02-16 12:38:21 +0900

    2022-02-16(수) project-app2 - 21

--- project-app2/app/src/main/java/com/eomcs/app2/App.java
+++ project-app2/app/src/main/java/com/eomcs/app2/App.java
@@ -125,6 +125,11 @@ public class App {
   }
 
   private void deleteScore() {
-
+    int no = promptInt("번호? ");
+    if (no < 0 || no >= scores.size()) {
+      System.out.println("번호가 유효하지 않습니다.");
+      return;
+    }
+    scores.remove(no);
   }
 }
