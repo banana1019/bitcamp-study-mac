b40f0b1a490dcf4099810b292e402163cdac61f7
banana1019 <jyho930@gmail.com>
2022-02-16 12:12:31 +0900

    2022-02-16(수) project-app2 - 18

--- project-app2/app/src/main/java/com/eomcs/app2/App.java
+++ project-app2/app/src/main/java/com/eomcs/app2/App.java
@@ -88,7 +88,19 @@ public class App {
   }
 
   private void detailScore() {
+    int no = promptInt("번호? ");
+    if (no < 0 || no >= scores.size()) {
+      System.out.println("번호가 유효하지 않습니다.");
+      return;
+    }
 
+    Score score = scores.get(no);
+    System.out.printf("이름: %s\n", score.getName());
+    System.out.printf("국어: %d\n", score.getKor());
+    System.out.printf("영어: %d\n", score.getEng());
+    System.out.printf("수학: %d\n", score.getMath());
+    System.out.printf("합계: %d\n", score.getSum());
+    System.out.printf("평균: %.1f\n", score.getAverage());
   }
 
   private void updateScore() {
