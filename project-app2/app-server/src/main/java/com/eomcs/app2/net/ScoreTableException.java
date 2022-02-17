d0e5291d07d58ed2435f1cf27d832adc82fbc930
banana1019 <jyho930@gmail.com>
2022-02-17 10:58:42 +0900

    2022-02-17(목) project-app2-server ScoreTableException

--- project-app2/app-server/src/main/java/com/eomcs/app2/net/ScoreTableException.java
+++ project-app2/app-server/src/main/java/com/eomcs/app2/net/ScoreTableException.java
@@ -1,5 +1,11 @@
 package com.eomcs.app2.net;
 
+// 서버의 ScoreTable을 다루다가 예외가 발생했을 때
+// 그 상황을 직관적으로 알 수 있도록 
+// 별도의 서브 클래스를 정의하였다.
+// => 즉 예외가 발생했을 때 예외 클래스 이름만으로도 
+//    어떤 상황에서 발생된 예외인지 바로 알 수 있도록 하기 위함이다.
+//
 public class ScoreTableException extends RuntimeException {
   private static final long serialVersionUID = 1L;
 
