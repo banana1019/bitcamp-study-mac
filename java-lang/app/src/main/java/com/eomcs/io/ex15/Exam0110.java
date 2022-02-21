b7e9aa740c8e295b8f71262c02276c81e77fa054
banana1019 <jyho930@gmail.com>
2022-02-21 11:09:50 +0900

    2022-02-21(월) - 2

--- java-lang/app/src/main/java/com/eomcs/io/ex15/Exam0110.java
+++ java-lang/app/src/main/java/com/eomcs/io/ex15/Exam0110.java
@@ -2,6 +2,7 @@
 package com.eomcs.io.ex15;
 
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.Base64.Encoder;
@@ -11,6 +12,7 @@ public class Exam0110 {
 
     Encoder encoder = Base64.getEncoder();
     FileInputStream in = new FileInputStream("./temp/aaa.png");
+    FileWriter out = new FileWriter("./temp/aaa.txt");
 
     byte[] buf = new byte[1024];
     int len = 0;
@@ -20,8 +22,12 @@ public class Exam0110 {
       // 바이트 배열에 저장된 바이너리 데이터를 텍스트로 변환하기
       String encodedStr = encoder.encodeToString(Arrays.copyOf(buf, len));
       System.out.println(encodedStr);
+
+      // 텍스트로 변환된 데이터를 파일로 출력하기
+      out.write(encodedStr);
     }
 
     in.close();
+    out.close();
   }
 }
