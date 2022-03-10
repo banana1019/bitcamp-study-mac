414006966ea8bf45da8898ef02d421ee673d19d6
banana1019 <jyho930@gmail.com>
2022-03-10 14:43:44 +0900

    2022-03-10(목) dao.mariadb 패키지 생성

--- mylist-boot/app/src/main/java/com/eomcs/mylist/dao/mariadb/JdbcBoardDao.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/dao/mariadb/JdbcBoardDao.java
@@ -1,4 +1,4 @@
-package com.eomcs.mylist.dao;
+package com.eomcs.mylist.dao.mariadb;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,6 +7,8 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Repository;
+import com.eomcs.mylist.dao.BoardDao;
+import com.eomcs.mylist.dao.DaoException;
 import com.eomcs.mylist.domain.Board;
 
 // @Repository
