7df5dc3757e0ab688bfb396c415d16235cb3404f
banana1019 <jyho930@gmail.com>
2022-03-17 10:22:56 +0900

    2022-03-17(목) myQuery 함수 만들기 - 3

--- mylist-boot/app/src/main/resources/static/myQuery.js
+++ mylist-boot/app/src/main/resources/static/myQuery.js
@@ -1,8 +1,31 @@
-function myQuery(selector) {
-  var e = document.querySelector(selector);
+function myQuery(selector, parent) {
   
+  if (parent == null) {
+    // 부모 태그가 지정되어 있으면 그 부모 태그 아래에서 조건에 해당하는 태그를 찾는다. 
+    var e = document.querySelector(selector);
+  } else {
+    // 부모 태그가 지정되어 있지 않으면 HTML 문서 전체에서 조건에 해당하는 태그를 찾는다
+    var e = document.querySelector(selector);
+  }
+  
+  // 태그에 스타일 설정
   e.css = function(name, value) {
     e.style[name] = value;
+  };
+  
+  // 태그 아래에서 다른 태그 찾기
+  e.find = function(selector) {
+    return myQuery(selector, e);
+  };
+  
+  // 태그의 콘텐트(innerHTML 프로퍼티 값)를 꺼내기 => <태그명>콘텐트</태그명>
+  e.html = function() {
+    return e.innerHTML;
+  }
+  
+  // 입력 상자의 value 프로퍼티 값을 설정하기
+  e.val = function(v) {
+    e.value = v;
   };
   
   return e;
