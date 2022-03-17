b1fe268f6abe3e224a9fe7dfe2500533c18a7919
banana1019 <jyho930@gmail.com>
2022-03-17 13:58:24 +0900

    2022-03-17(목) myQuery 함수 만들기 - 7

--- mylist-boot/app/src/main/resources/static/myQuery.js
+++ mylist-boot/app/src/main/resources/static/myQuery.js
@@ -1,22 +1,21 @@
 function myQuery(selector, parent) {
   
   if (typeof(selector) == "string") {
-    if (selector.startsWith("<")) { // <address>
-      // 파라미터 값이 '<'로 시작한다면 태그를 생성한다.
-      var e = document.createElement(selector.substring(1, selector.length - 1))
+    if (selector.startsWith("<")) {
+      // 파라미티 값이 '<' 시작한다면 태그를 생성한다.
+      var e = document.createElement(selector.substring(1, selector.length - 1));
     } else if (parent != null) {
-      // 부모 태그가 지정되어 있으면 그 부모 태그 아래에서 조건에 해당하는 태그를 찾는다. 
+      // 부모 태그가 지정되어 있으면 그 부모 태그 아래에서 조건에 해당하는  태그를 찾는다. 
       var e = parent.querySelector(selector);
     } else {
       // 부모 태그가 지정되어 있지 않으면 HTML 문서 전체에서 조건에 해당하는 태그를 찾는다.
       var e = document.querySelector(selector);
     }
-    } else if (selector instanceof HTMLElement) {
-      var e = selector;
-    }
+  } else if (selector instanceof HTMLElement) {
+    var e = selector;
+  }
   
-  
-  // 태그에 스타일 값 조회 및 설정
+  // 태그의 스타일 값 조회 및 설정
   e.css = function(name, value) {
     if (arguments.length == 1) {
       return e.style[name];
@@ -31,7 +30,7 @@ function myQuery(selector, parent) {
     return myQuery(selector, e);
   };
   
-  // 태그의 콘텐트(innerHTML 프로퍼티 값)를 조회하고 꺼내기 => <태그명>콘텐트</태그명>
+  // 태그의 콘텐트(innerHTML 프로퍼티 값)를 조회하고 꺼내기
   e.html = function(content) {
     if (arguments.length == 0) {
       return e.innerHTML;
@@ -43,7 +42,6 @@ function myQuery(selector, parent) {
   
   // 입력 상자의 value 프로퍼티 값을 조회하거나 설정하기(setter/getter)
   e.val = function(v) {
-    console.log(arguments)
     if (arguments.length == 0) {
       return e.value;
     } else {
@@ -52,11 +50,11 @@ function myQuery(selector, parent) {
     }
   };
   
-  // 태그의 속성 값을 조회하거나 설정하기(setter/getter)
+  // 태그의 속성 값을 조회하고 설정하기(getter/setter)
   e.attr = function(name, value) {
     if (arguments.length == 1) {
       return e.getAttribute(name);
-    } else if ( arguments.length == 2) {
+    } else if (arguments.length == 2) {
       e.setAttribute(name, value);
       return e;
     }
@@ -70,11 +68,28 @@ function myQuery(selector, parent) {
   // 현재 태그를 부모 태그에 자식 태그로 만든다.
   e.appendTo = function(parent) {
     parent.appendChild(e);
-  }
+  };
+  
+  // 현재 태그에 클래스를 추가한다.
+  e.addClass = function(name) {
+    e.classList.add(name);
+    return e;
+  };
+  
+  // 현재 태그에서 해당 클래스를 제거한다.
+  e.removeClass = function(name) {
+    e.classList.remove(name);
+    return e;
+  };
+  
+  // keyup 이벤트 리스너 등록한다.
+  e.keyup = function(listener) {
+    e.addEventListener("keyup", listener);
+    return e;
+  };
   
   return e;
 }
-
 
  
  
