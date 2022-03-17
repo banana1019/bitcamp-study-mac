7e76402ec4a6e9717c423f1f21284e5d9a64ecbf
banana1019 <jyho930@gmail.com>
2022-03-17 10:01:39 +0900

    2022-03-17(목) myQuery 함수 만들기 - 2

--- mylist-boot/app/src/main/resources/static/myQuery.js
+++ mylist-boot/app/src/main/resources/static/myQuery.js
@@ -1,4 +1,11 @@
 function myQuery(selector) {
   var e = document.querySelector(selector);
+  
+  e.css = function(name, value) {
+    e.style[name] = value;
+  };
+  
   return e;
 }
+
+var $ = myQuery;
