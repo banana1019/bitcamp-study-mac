a23624161a3469f4b869d3479afd79854eadc56e
banana1019 <jyho930@gmail.com>
2022-03-17 11:44:57 +0900

    2022-03-17(목) myQuery 함수 만들기 - 5

--- mylist-boot/app/src/main/resources/static/myQuery.js
+++ mylist-boot/app/src/main/resources/static/myQuery.js
@@ -1,16 +1,28 @@
 function myQuery(selector, parent) {
   
-  if (parent != null) {
-    // 부모 태그가 지정되어 있으면 그 부모 태그 아래에서 조건에 해당하는  태그를 찾는다. 
-    var e = parent.querySelector(selector);
-  } else {
-    // 부모 태그가 지정되어 있지 않으면 HTML 문서 전체에서 조건에 해당하는 태그를 찾는다.
-    var e = document.querySelector(selector);
-  }
+  if (typeof(selector) == "string") {
+    if (selector.startsWith("<")) { // <address>
+      // 파라미터 값이 '<'로 시작한다면 태그를 생성한다.
+      var e = document.createElement(selector.substring(1, selector.length - 1))
+    } else if (parent != null) {
+      // 부모 태그가 지정되어 있으면 그 부모 태그 아래에서 조건에 해당하는 태그를 찾는다. 
+      var e = parent.querySelector(selector);
+    } else {
+      // 부모 태그가 지정되어 있지 않으면 HTML 문서 전체에서 조건에 해당하는 태그를 찾는다.
+      var e = document.querySelector(selector);
+    }
+    } else if (selector instanceof HTMLElement) {
+      var e = selector;
+    }
   
-  // 태그에 스타일 설정
+  
+  // 태그에 스타일 값 조회 및 설정
   e.css = function(name, value) {
-    e.style[name] = value;
+    if (arguments.length == 1) {
+      return e.style[name];
+    } else if (arguments.length == 2) {
+      e.style[name] = value;
+    }
   };
   
   // 태그 아래에서 다른 태그 찾기
@@ -18,23 +30,42 @@ function myQuery(selector, parent) {
     return myQuery(selector, e);
   };
   
-  // 태그의 콘텐트(innerHTML 프로퍼티 값)를 꺼내기 => <태그명>콘텐트</태그명>
-  e.html = function() {
-    return e.innerHTML;
+  // 태그의 콘텐트(innerHTML 프로퍼티 값)를 조회하고 꺼내기 => <태그명>콘텐트</태그명>
+  e.html = function(content) {
+    if (arguments.length == 0) {
+      return e.innerHTML;
+    } else if (arguments.length == 1) {
+      e.innerHTML = content;
+    }
   };
   
-  // 입력 상자의 value 프로퍼티 값을 설정하기
+  // 입력 상자의 value 프로퍼티 값을 조회하거나 설정하기(setter/getter)
   e.val = function(v) {
-    e.value = v;
+    console.log(arguments)
+    if (arguments.length == 0) {
+      return e.value;
+    } else {
+      e.value = v;
+    }
   };
   
-  // 태그의 속성 값을 설정하기
+  // 태그의 속성 값을 조회하거나 설정하기(setter/getter)
   e.attr = function(name, value) {
-    e.setAttribute(name, value);
+    if (arguments.length == 1) {
+      return e.getAttribute(name);
+    } else if ( arguments.length == 2) {
+      e.setAttribute(name, value);
+    }
+  };
+  
+  // 태그의 부모를 찾아 리턴한다.
+  e.parent = function() {
+    return myQuery(e.parentNode);
   };
   
   return e;
 }
+
 
  
  
