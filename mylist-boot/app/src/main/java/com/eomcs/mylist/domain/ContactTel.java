5fcd62e540272cd696c637863f6320349752446d
banana1019 <jyho930@gmail.com>
2022-03-18 10:48:36 +0900

    2022-03-18(금) ContactTel 도메인 - 2

--- mylist-boot/app/src/main/java/com/eomcs/mylist/domain/ContactTel.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/domain/ContactTel.java
@@ -5,4 +5,36 @@ public class ContactTel {
   int contactNo;
   int telTypeNo;
   String tel;
+
+  @Override
+  public String toString() {
+    return "ContactTel [no=" + no + ", contactNo=" + contactNo + ", telTypeNo=" + telTypeNo
+        + ", tel=" + tel + "]";
+  }
+
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public int getContactNo() {
+    return contactNo;
+  }
+  public void setContactNo(int contactNo) {
+    this.contactNo = contactNo;
+  }
+  public int getTelTypeNo() {
+    return telTypeNo;
+  }
+  public void setTelTypeNo(int telTypeNo) {
+    this.telTypeNo = telTypeNo;
+  }
+  public String getTel() {
+    return tel;
+  }
+  public void setTel(String tel) {
+    this.tel = tel;
+  }
+
 }
