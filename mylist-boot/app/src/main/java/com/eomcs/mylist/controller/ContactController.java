7fdd1cc51c4f7a2af27d3a9c15f604c33da6b853
banana1019 <jyho930@gmail.com>
2022-03-18 14:06:02 +0900

    2022-03-18(금) ContactController 클래스 수정

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.eomcs.mylist.dao.ContactDao;
 import com.eomcs.mylist.domain.Contact;
+import com.eomcs.mylist.domain.ContactTel;
 
 @RestController 
 public class ContactController {
@@ -18,9 +19,13 @@ public class ContactController {
   }
 
   @RequestMapping("/contact/add")
-  public Object add(Contact contact) throws Exception {
+  public Object add(Contact contact, String[] tel) throws Exception {
     contactDao.insert(contact);
-    return contactDao.countAll();
+    for (int i = 0; i < tel.length; i++) {
+      String[] value = tel[i].split(",");
+      contactDao.insertTel(new ContactTel(contact.getNo(), Integer.parseInt(value[0]), value[1]));
+    }
+    return 1;
   }
 
   @RequestMapping("/contact/get")
