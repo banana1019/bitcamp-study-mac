1c9aaa63057c1d6414b0a842c66032424a314932
banana1019 <jyho930@gmail.com>
2022-03-29 19:25:54 +0900

    2022-03-29(화) 세션과 쿠키

--- mylist-boot/README.sql
+++ mylist-boot/README.sql
@@ -70,3 +70,15 @@ insert into ml_cont_tel(ct_no, contact_no, tt_no, tel)
 -- 도서록 테이블에 책 사진 파일 이름을 저장할 컬럼을 추가한다.
 alter table ml_book
   add column photo varchar(255);
+
+create table ml_member (
+  no int not null,
+  name varchar(50) not null,
+  email varchar(100) not null,
+  password varchar(255) not null,
+  regist_date datetime default now()
+);
+
+alter table ml_member
+  add constraint primary key (no),
+  modify column no int not null auto_increment;
