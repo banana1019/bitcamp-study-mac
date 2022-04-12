ff0408ffba1dc116dae2fb809d0e70c33b0a220d
banana1019 <jyho930@gmail.com>
2022-04-12 12:48:04 +0900

    2022-04-12(화) MyList를 서블릿으로 프로그래밍 하기

--- project-app3/app/src/main/java/com/eomcs/mylist/web/board/BoardDetailServlet.java
+++ project-app3/app/src/main/java/com/eomcs/mylist/web/board/BoardDetailServlet.java
@@ -2,31 +2,137 @@ package com.eomcs.mylist.web.board;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.eomcs.mylist.domain.Board;
+import com.eomcs.mylist.service.BoardService;
 
-// 서블릿 컨테이너가 실행할 클래스를 만드려면
-// Servlet API 규칙에 따라 작성해야 한다.
-//
 @SuppressWarnings("serial")
-@WebServlet("/board/detail") // 서블릿 컨테이너에게 이 클래스가 /hello 요청을 처리하는 서블릿임을 알려준다.
+@WebServlet("/board/detail") 
 public class BoardDetailServlet extends HttpServlet {
+
+  BoardService boardService;
+
+  @Override
+  public void init() throws ServletException {
+    // BoardService 객체를 웹애플리케이션 보관소에서 꺼낸다.
+    ServletContext 웹애플리케이션보관소 = this.getServletContext();
+    boardService = (BoardService) 웹애플리케이션보관소.getAttribute("boardService");
+  }
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
 
-    // HTTP 클라이언트에게 보낼 콘텐트의 MIME 타입을 설정한다.
-    resp.setContentType("text/plain;charset=UTF-8");
-
-    // HTTP 클라이언트에게 콘텐트를 출력할 도구를 준비한다.
+    resp.setContentType("text/html;charset=UTF-8");
     PrintWriter out = resp.getWriter();
 
-    // HTTP 클라이언트에게 콘텐트를 출력한다.
-    out.println("게시글 상세보기");
-  }
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("  <meta charset=\"UTF-8\">");
+    out.println("  <title>MyList!</title>");
+    out.println("  <link href=\"/css/common.css\" rel=\"stylesheet\">");
+    out.println("</head>");
+    out.println("<body>");
 
+    out.println("<div class=\"container\">");
+
+    out.println("<div id=\"header\">");
+    out.println("<style>");
+
+    out.println("#login-btn {");
+    out.println("  position: absolute;");
+    out.println("  right: 10px;");
+    out.println("}");
+
+    out.println("#logout-btn {");
+    out.println("  position: absolute;");
+    out.println("  right: 10px;");
+    out.println("}");
+
+    out.println("#app-title {");
+    out.println("  font-size: 1.5em;");
+    out.println("  font-weight: bold;");
+    out.println("  font-style: none;");
+    out.println("  color: white;");
+    out.println("} ");
+
+    out.println("#user-name {");
+    out.println("  position: absolute;");
+    out.println("  right: 90px;");
+    out.println("}");
+    out.println("</style>");
+
+    out.println("<a href=\"/index.html\"><span id=\"app-title\">MyList</span></a> ");
+    out.println("<button id=\"login-btn\" type=\"button\" class=\"not-login\">로그인</button>");
+    out.println("<span id=\"user-name\" class=\"login\"></span>");
+    out.println("<button id=\"logout-btn\" type=\"button\" class=\"login\">로그아웃</button>  ");
+    out.println("</div>");
+
+    out.println("<div id=\"sidebar\">");
+    out.println("<style>");
+    out.println("h1.sidebar {");
+    out.println("  font-size: 1.2em;");
+    out.println("}");
+    out.println("</style>");
+
+    out.println("<h1 class=\"sidebar\">제목</h1>");
+    out.println("<div class=\"sidebar\">");
+    out.println("<ul>");
+    out.println("  <li>내용1</li>");
+    out.println("  <li>내용2</li>");
+    out.println("  <li>내용3</li>");
+    out.println("</ul>");
+    out.println("</div>  ");
+    out.println("</div>");
+
+    out.println("<div id=\"content\">");
+    out.println("<h1>게시글 상세</h1>");
+
+    int no = Integer.parseInt(req.getParameter("no"));
+    Board board = boardService.get(no);
+
+    out.println("<form action='update' method='post'>");
+    out.printf("번호: <input name=\"no\" type=\"text\" value='%d' readonly><br>\n", board.getNo());
+    out.printf("제목*: <input name=\"title\" type=\"text\" value='%s'><br>\n", board.getTitle());
+    out.printf("내용*: <textarea name=\"content\" cols=\"50\" rows=\"10\">%s</textarea><br>\n", board.getContent());
+    out.printf("작성자: <span>%s</span><br>\n", board.getWriter().getName());
+    out.printf("조회수: <span>%d</span><br>\n", board.getViewCount());
+    out.printf("등록일: <span>%s</span><br>\n", board.getCreatedDate());
+    out.println("별표(*) 항목은 필수 입력입니다.<br>");
+    out.println("<button>변경</button>");
+    out.println("<button type=\"button\">삭제</button>");
+    out.println("<button type=\"button\">취소</button>");
+    out.println("</form>");
+    out.println("</div>");
+
+    out.println("<div id=\"footer\">");
+    out.println("<style>");
+    out.println("#company-title {");
+    out.println("  font-size: 1.2em;");
+    out.println("  font-weight: bold;");
+    out.println("}");
+
+    out.println("#company-address {");
+    out.println("  display: inline-block;");
+    out.println("  width: calc(100% - 100px); ");
+    out.println("  text-align: center; ");
+    out.println("}");
+    out.println("</style>");
+
+    out.println("<span id=\"company-title\">비트캠프</span> ");
+    out.println("<address id=\"company-address\">서울 강남구 강남대로94길 20, 삼오빌딩</address>  ");
+    out.println("</div>");
+
+    out.println("</div>");
+
+    out.println("</body>");
+    out.println("</html>");
+
+  }
 }
--- project-app3/app/src/main/java/com/eomcs/mylist/web/listener/ContextLoaderListener.java
+++ project-app3/app/src/main/java/com/eomcs/mylist/web/listener/ContextLoaderListener.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -12,16 +13,18 @@ import com.eomcs.mylist.service.MemberService;
 import com.eomcs.mylist.service.impl.DefaultBoardService;
 import com.eomcs.mylist.service.impl.DefaultMemberService;
 
-// - 웹 애플리케이션이 시작될 때 서비스 객체, DAO 객체, MyBatis 객체를 준비한다.
+// 역할:
+// - 웹애플리케이션이 시작될 때 서비스 객체, DAO 객체, Mybatis 객체를 준비한다.
+// 
+@WebListener
 public class ContextLoaderListener implements ServletContextListener {
-
   @Override
   public void contextInitialized(ServletContextEvent sce) {
-    // 웹 애플리케이션이 시작되면 이 메서드가 호출될 것이다.
-    System.out.println("서비스 및 DAO, MyBatis 객체 준비");
+    // 웹애플리케이션이 시작되면 이 메서드가 호출될 것이다.
+    System.out.println("서비스 및 DAO, Mybatis 객체 준비!");
 
     try {
-      // 1) MyBatis의 SqlSessionFactory 준비
+      // 1) Mybatis의 SqlSessionFactory 준비
       String resource = "com/eomcs/mylist/conf/mybatis-config.xml";
       InputStream inputStream = Resources.getResourceAsStream(resource);
       SqlSessionFactory sqlSessionFactory =
@@ -29,15 +32,24 @@ public class ContextLoaderListener implements ServletContextListener {
 
       // 2) 서비스 객체 생성
       BoardService boardService = new DefaultBoardService(sqlSessionFactory);
-      MemberService memberSerive = new DefaultMemberService(sqlSessionFactory);
+      MemberService memberService = new DefaultMemberService(sqlSessionFactory);
 
       // 3) 서블릿에서 서비스 객체를 사용할 수 있도록 ServletContext 보관소에 저장한다.
       ServletContext 웹애플리케이션보관소 = sce.getServletContext();
       웹애플리케이션보관소.setAttribute("boardService", boardService);
-      웹애플리케이션보관소.setAttribute("memberSerive", memberSerive);
+      웹애플리케이션보관소.setAttribute("memberService", memberService);
 
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
 }
+
+
+
+
+
+
+
+
+
