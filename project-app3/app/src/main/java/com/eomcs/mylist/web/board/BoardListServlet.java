245bfb279eea2c6608d701c6b921af73f971e783
banana1019 <jyho930@gmail.com>
2022-04-12 11:38:35 +0900

    2022-04-12(화) MyList를 서블릿으로 프로그래밍 하기

--- project-app3/app/src/main/java/com/eomcs/mylist/web/board/BoardListServlet.java
+++ project-app3/app/src/main/java/com/eomcs/mylist/web/board/BoardListServlet.java
@@ -2,11 +2,15 @@ package com.eomcs.mylist.web.board;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.eomcs.mylist.domain.Board;
+import com.eomcs.mylist.service.BoardService;
 
 // 서블릿 컨테이너가 실행할 클래스를 만드려면
 // Servlet API 규칙에 따라 작성해야 한다.
@@ -15,9 +19,22 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/board/list") // 서블릿 컨테이너에게 이 클래스가 /hello 요청을 처리하는 서블릿임을 알려준다.
 public class BoardListServlet extends HttpServlet {
 
+  BoardService boardService;
+
+  @Override
+  public void init() throws ServletException {
+    ServletContext 웹애플리케이션보관소 = this.getServletContext();
+    boardService = (BoardService) 웹애플리케이션보관소.getAttribute("boardService");
+  }
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
+
+    List<Board> boards = boardService.list();
+    for (Board board : boards) {
+      System.out.println(board.getTitle());
+    }
 
     // HTTP 클라이언트에게 보낼 콘텐트의 MIME 타입을 설정한다.
     resp.setContentType("text/plain;charset=UTF-8");
--- project-app3/app/src/main/java/com/eomcs/mylist/web/listener/ContextLoaderListener.java
+++ project-app3/app/src/main/java/com/eomcs/mylist/web/listener/ContextLoaderListener.java
@@ -1,5 +1,43 @@
 package com.eomcs.mylist.web.listener;
 
+import java.io.InputStream;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import com.eomcs.mylist.service.BoardService;
+import com.eomcs.mylist.service.MemberService;
+import com.eomcs.mylist.service.impl.DefaultBoardService;
+import com.eomcs.mylist.service.impl.DefaultMemberService;
+
+// - 웹 애플리케이션이 시작될 때 서비스 객체, DAO 객체, MyBatis 객체를 준비한다.
 public class ContextLoaderListener implements ServletContextListener {
 
+  @Override
+  public void contextInitialized(ServletContextEvent sce) {
+    // 웹 애플리케이션이 시작되면 이 메서드가 호출될 것이다.
+    System.out.println("서비스 및 DAO, MyBatis 객체 준비");
+
+    try {
+      // 1) MyBatis의 SqlSessionFactory 준비
+      String resource = "com/eomcs/mylist/conf/mybatis-config.xml";
+      InputStream inputStream = Resources.getResourceAsStream(resource);
+      SqlSessionFactory sqlSessionFactory =
+          new SqlSessionFactoryBuilder().build(inputStream);
+
+      // 2) 서비스 객체 생성
+      BoardService boardService = new DefaultBoardService(sqlSessionFactory);
+      MemberService memberSerive = new DefaultMemberService(sqlSessionFactory);
+
+      // 3) 서블릿에서 서비스 객체를 사용할 수 있도록 ServletContext 보관소에 저장한다.
+      ServletContext 웹애플리케이션보관소 = sce.getServletContext();
+      웹애플리케이션보관소.setAttribute("boardService", boardService);
+      웹애플리케이션보관소.setAttribute("memberSerive", memberSerive);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
